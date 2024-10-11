0f0d6b0b04063b25193333a5245684205941c03a
ENNEDDI-Omar <enneddiomar@gmail.com>
2024-10-11 17:05:41 +0200

    integration de user et role

--- src/main/java/org/tasking/domain/entities/Role.java
+++ src/main/java/org/tasking/domain/entities/Role.java
@@ -1,4 +1,46 @@
 package org.tasking.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private List<User> users = new ArrayList<>();
+
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/org/tasking/domain/entities/User.java
+++ src/main/java/org/tasking/domain/entities/User.java
@@ -21,5 +21,67 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false) // clé étrangère pour la relation avec Role
+    private Role role;
 
+    public User() {}
+
+    public User(String username, String email, String password, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role.getName() + '\'' +
+                '}';
+    }
 }
