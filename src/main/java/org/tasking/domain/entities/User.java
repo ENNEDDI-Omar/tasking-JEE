4a4b183023968d1b237144532d14326d18c81828
ENNEDDI-Omar <enneddiomar@gmail.com>
2024-10-14 19:29:40 +0200

    auth config

--- src/main/java/org/tasking/domain/entities/User.java
+++ src/main/java/org/tasking/domain/entities/User.java
@@ -19,7 +19,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @ManyToOne
